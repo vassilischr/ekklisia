e1796403522efbf6692101c0ea93408f709cd282
vassilischr <64667124+vassilischr@users.noreply.github.com>
2020-05-02 02:00:11 +0300

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /death_files/counter.js was not found on this server.</p>
+<p>Additionally, a 404 Not Found
+error was encountered while trying to use an ErrorDocument to handle the request.</p>
+<hr>
+<address>Apache/2.2.16 (Unix) mod_ssl/2.2.16 OpenSSL/0.9.8e-fips-rhel5 mod_mono/2.6.3 FrontPage/5.0.2.2635 mod_bwlimited/1.4 mod_auth_passthrough/2.1 mod_wsgi/3.2 Python/2.4.3 Server at ekklisia.heliohost.org Port 80</address>
+</body></html>
