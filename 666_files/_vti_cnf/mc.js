463ea371d6be1e6d9dd5e2ddd371f88059b86437
vassilischr <64667124+vassilischr@users.noreply.github.com>
2020-05-02 15:07:26 +0300

    folders
    folders

---
+++ 666_files/_vti_cnf/mc.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Jan 2017 17:18:10 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_thicketsupportingfile:BW|true
+vti_cacheddtm:TX|27 Jan 2017 17:18:10 -0000
+vti_filesize:IR|407
+vti_backlinkinfo:VX|
